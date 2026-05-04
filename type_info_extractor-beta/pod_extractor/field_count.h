ca7be623d238f2e1947534ae3cc9406388e24d4b
eduard.borovitski <eduard.borovitski@myoffice.team>
2026-05-04 19:14:36 +0300

    field count clean up

--- type_info_extractor-beta/pod_extractor/field_count.h
+++ type_info_extractor-beta/pod_extractor/field_count.h
@@ -3,25 +3,26 @@
 #include <type_traits>
 #include <utility>
 
+#include "mp.h"
+
 template<std::size_t N>
 struct accept_any_type
 {
-    std::size_t ignore;
     template <class Type> constexpr operator Type&() const noexcept;
 };
 
 template<typename T, std::size_t... I>
 constexpr auto check_impl(std::index_sequence<I...>) 
-    -> decltype(T{ accept_any_type<I>{I}... } );
+    -> decltype(T{ accept_any_type<I>{}... } );
 
+    
 template<typename T, std::size_t N, typename = void>
 struct can_init_with : std::false_type {};
 
 template<typename T, std::size_t N>
 struct can_init_with<T, N, std::void_t<decltype(check_impl<T>(std::make_index_sequence<N>()))>>
     : std::true_type {};
-template <std::size_t I>
-using size_t_i_c = std::integral_constant<std::size_t, I>;
+
 
 template<class T>
 struct count_type_fields
@@ -36,7 +37,6 @@ private:
     count(size_t_i_c<lowerBound>, size_t_i_c<upperBound>) noexcept
     {
         if constexpr(can_init_with<T, upperBound>::value)
-        // if constexpr(sizeof(decltype_t<T, upperBound>) != 0)
         {
             return count(size_t_i_c<upperBound>{}, size_t_i_c<upperBound +
                         (upperBound - lowerBound + 1) / 2>{});
@@ -47,14 +47,13 @@ private:
                      size_t_i_c<(lowerBound + upperBound) / 2>{});
         }
     }
-    static constexpr std::size_t EST_MAX_COUNT()
-    { return (sizeof(T) / 16) + 1; } //works faster
+    static constexpr std::size_t EST_MAX_COUNT = (sizeof(T) / 16) + 1; //works faster
 public:
     static constexpr std::size_t count_fields() noexcept
     {
-        return count(size_t_i_c<0>{}, size_t_i_c<EST_MAX_COUNT()>{});
+        return count(size_t_i_c<0>{}, size_t_i_c<EST_MAX_COUNT>{});
     }
-    enum { result = count_fields() };
+    static constexpr std::size_t result = count_fields();
 };
 
 template<class T>
